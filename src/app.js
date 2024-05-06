6f318f9b16c925c0e95dcb37b7e2af43ca8b0eb9
Alphamikey23 <140776901+Alphamikey23@users.noreply.github.com>
2024-05-06 14:51:11 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const foods = ["pizza", "burger", "fingerChips", "donuts", "springRoll"];
+const numberArray = [12,324,213,4,2,3,45,4234];
+const myArray = [11, 34, 20, 5, 53, 16];
+
+// Progression 1:
+modifiedfood = foods.slice(1,3);
+console.log(modifiedfood);
+
+
+// Progression 2:
+
+
+// Progression 3:
+
+
+// Progression 4:
+
+
+// Progression 5:
+
+
+// Progression 6:
+
+
+// Progression 7:
+
+
